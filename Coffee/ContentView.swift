6617b32ff8bc441fe00b2ef060988b0e3f6fea68
Alan <guerrero_alan@outlook.com>
2019-07-17 19:19:13 -0500

    first commit

---
+++ Coffee/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Coffee
+//
+//  Created by Alan Guerrero on 7/17/19.
+//  Copyright © 2019 Alan Guerrero. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
